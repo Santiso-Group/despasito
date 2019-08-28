ed90a22fac95defd61b35dca1e77827e1e816a43
Jennifer A Clark <jennifer.clark@gnarlyoak.com>
2019-08-28 18:03:10 -0400

    Mock importlib, not supported by readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['deap', 'solv_assoc', 'solv_assoc_matrix']
+MOCK_MODULES = ['deap', 'solv_assoc', 'solv_assoc_matrix',"importlib"]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
