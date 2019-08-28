d3c0f292037c49e7a03108967ac3be36f3a24372
Jennifer A Clark <jennifer.clark@gnarlyoak.com>
2019-08-28 17:41:28 -0400

    Added mock fortran modules to appease readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,15 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import despasito
 
+from unittest.mock import MagicMock
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['deap', 'solv_assoc', 'solv_assoc_matrix']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- Project information -----------------------------------------------------
 
