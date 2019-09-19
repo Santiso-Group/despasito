803a1f039553a5339239d0c92a74a887d9596660
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-09-19 16:19:13 -0400

    kwargs stores --jit flag

--- despasito/__main__.py
+++ despasito/__main__.py
@@ -12,6 +12,7 @@ parser.add_argument("-v", "--verbose", action="count", default=0, help="Verbose 
 parser.add_argument("--log", nargs='?', dest="logFile", default="despasito.log", help="Output a log file. The default name is despasito.log.")
 parser.add_argument("-t", "--threads", dest="threads", type=int, help="Set the number of theads used. This hasn't been implemented yet.",default=1)
 parser.add_argument("-p", "--path", default=".", help="Set the location of the data/library files (e.g. SAFTcross, etc.) for despasito to look for")
+parser.add_argument("--jit", action='store_true', default=0, help="turn on Numba's JIT compilation for accelerated computation")
 
 ## Extract arguements
 args = parser.parse_args()
@@ -25,8 +26,8 @@ if args.logFile:
     ## Set up logging (refined after argparse)
     logger = logging.getLogger()
     logger.setLevel(args.verbose)
-    # Set up rotating log files
 
+    # Set up rotating log files
     log_file_handler = logging.handlers.TimedRotatingFileHandler(args.logFile, when='M', interval=2)
     log_file_handler.setFormatter( logging.Formatter('%(asctime)s [%(levelname)s](%(name)s:%(funcName)s:%(lineno)d): %(message)s') )
     log_file_handler.setLevel(args.verbose)
@@ -36,11 +37,13 @@ if args.logFile:
     console_handler = logging.StreamHandler() # sys.stderr
     console_handler.setLevel(args.verbose) # set later by set_log_level_from_verbose() in interactive sessions
     console_handler.setFormatter( logging.Formatter('[%(levelname)s](%(name)s): %(message)s') )
-    logger.addHandler(console_handler)
-
     console_handler.setLevel(args.verbose)
 
+    #logger.addHandler(console_handler)
+
+
     logging.info("Input args: %r", args)
+    logging.info("JIT compilation: %r", bool(args.jit))
 
 # Threads
 # if args.threads != None:
@@ -57,6 +60,7 @@ else:
 kwargs["logFile"] = args.logFile
 kwargs["threads"] = args.threads
 kwargs["path"] = args.path
+kwargs["jit" ] = args.jit
 
 run(**kwargs)
 
