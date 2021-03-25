Bug Fix, adjust weights array in parameter fitting when calculation result is NaN

despasito/parameter_fitting/fit_functions.py
@@ -519,27 +519,37 @@ def obj_function_form(
             if not np.isnan((data_test[i] - data0[i]) / data0[i])
         ]
     )
+    if gtb.isiterable(weights):
+        weight_tmp = np.array(
+            [
+                weights[i]
+                for i in range(len(data_test))
+                if not np.isnan((data_test[i] - data0[i]) / data0[i])
+            ]
+        )
+    else:
+        weight_tmp = weights
 
     if method == "average-squared-deviation":
-        obj_value = np.mean(data_tmp ** 2 * weights)
+        obj_value = np.mean(data_tmp ** 2 * weight_tmp)
 
     elif method == "sum-squared-deviation":
-        obj_value = np.sum(data_tmp ** 2 * weights)
+        obj_value = np.sum(data_tmp ** 2 * weight_tmp)
 
     elif method == "sum-squared-deviation-boltz":
         data_min = np.min(data_tmp)
         obj_value = np.sum(
-            data_tmp ** 2 * weights * np.exp((data_min - data_tmp) / np.abs(data_min))
+            data_tmp ** 2 * weight_tmp * np.exp((data_min - data_tmp) / np.abs(data_min))
         )
 
     elif method == "sum-deviation-boltz":
         data_min = np.min(data_tmp)
         obj_value = np.sum(
-            data_tmp * weights * np.exp((data_min - data_tmp) / np.abs(data_min))
+            data_tmp * weight_tmp * np.exp((data_min - data_tmp) / np.abs(data_min))
         )
 
     elif method == "percent-absolute-average-deviation":
-        obj_value = np.mean(np.abs(data_tmp) * weights) * 100
+        obj_value = np.mean(np.abs(data_tmp) * weight_tmp) * 100
 
     if len(data_tmp) == 0:
         obj_value = np.nan