c8e68dc3049acdd9462d647405ec365c86109215
Jennifer A Clark <jennifer.clark@gnarlyoak.com>
2021-04-04 09:35:25 -0400

    Bug fix grid_minimization

--- despasito/parameter_fitting/constraint_types.py
+++ despasito/parameter_fitting/constraint_types.py
@@ -5,6 +5,7 @@
 def type1(x, *args):
     """ Example constraint. Notice that if this is used by a constraint class, there are no other args, while there are for dict constraints.
     """
-    # NoteHere
+
+    output = None
 
     return output
