d8fc6d8c4d5750729e60baf68eef776a9c6fa40f
Jennifer A Clark <jennifer.a.clark13@gmail.com>
2024-01-10 09:35:27 -0500

    Update liquid_density.py

--- despasito/parameter_fitting/data_classes/liquid_density.py
+++ despasito/parameter_fitting/data_classes/liquid_density.py
@@ -34,8 +34,10 @@ class Data(ExpDataTemplate):
         * calculation_type (str) - Optional, default='liquid_properties'
         * MultiprocessingObject (obj) - Optional, Initiated :class:`~despasito.utils.parallelization.MultiprocessingJob`
         * eos_obj (obj) - Equation of state object
-        * T (list) - List of temperature values for calculation
+        * T (list) - [K] List of temperature values for calculation
+        * P (list) - [Pa] List of pressure values for calculation
         * xi (list) - List of liquid mole fractions used in liquid_properties calculations
+        * rhol (list) - [mol/:math:`m^3`] Evaluated liquid density values
         * weights (dict) - A dictionary where each key is a system constraint (e.g. T or xi) which is also a header used in an optional exp. data file. The value associated with a header can be a list as long as the number of data points to multiply by the objective value associated with each point, or a float to multiply the objective value of this data set.
         * objective_method (str) - The 'method' keyword in function despasito.parameter_fitting.fit_functions.obj_function_form.
         * density_opts (dict) - Optional, default={"min_density_fraction":(1.0 / 60000.0), "density_increment":10.0, "max_volume_increment":1.0E-4}, Dictionary of options used in calculating pressure vs. mole fraction curves.
