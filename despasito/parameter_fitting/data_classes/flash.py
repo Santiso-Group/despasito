315666ad30ef586db456ba8a3a9c679758a1c7bf
Jennifer A Clark <jennifer.a.clark13@gmail.com>
2024-01-10 09:37:59 -0500

    Update flash.py

--- despasito/parameter_fitting/data_classes/flash.py
+++ despasito/parameter_fitting/data_classes/flash.py
@@ -35,6 +35,8 @@ class Data(ExpDataTemplate):
         * eos_obj (obj) - Equation of state object
         * T (list) - List of temperature values for calculation
         * P (list) - List of pressure values for calculation
+        * xi (list) - List of liquid compositions
+        * yi (list) - List of vapor compositions
         * weights (dict) - A dictionary where each key is a system constraint (e.g. T or xi) which is also a header used in an optional exp. data file. The value associated with a header can be a list as long as the number of data points to multiply by the objective value associated with each point, or a float to multiply the objective value of this data set.
         * density_opts (dict) - Optional, default={"min_density_fraction":(1.0 / 300000.0), "density_increment":10.0, "max_volume_increment":1.0E-4}, Dictionary of options used in calculating pressure vs. mole fraction curves.
         * kwargs for :func:`~despasito.parameter_fitting.fit_functions.obj_function_form`
